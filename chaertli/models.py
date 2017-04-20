13541b03021217b8132f8747ba81585b7d9f5c3d
MonaLuna <sara.kuhnert@hotmail.com>
2017-04-20 19:46:08 +0200

    defines Chaertli model

--- chaertli/models.py
+++ chaertli/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Chaertli(models.Model):
+	
+	created = models.DateField(auto_now_add=True)
+
+	def __str__(self):
+		return self.created.strftime("%Y-%m-%d")
