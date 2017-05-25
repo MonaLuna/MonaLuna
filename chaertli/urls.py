18e90d6f11e314a2769e8d6d3468964a9fd6a407
MonaLuna <sara.kuhnert@hotmail.com>
2017-05-25 16:36:37 +0200

    Adds detail view
    ...and defines its url endpoint
    ...and adds detail template

--- chaertli/urls.py
+++ chaertli/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='chaertli-index'),
     url(r'^list/$', views.ChaertliListView.as_view(), name='chaertli-list'),
+    url(r'^(?P<pk>[0-9]+)/$', views.ChaertliDetailView.as_view(), name='chaertli-detail')
 ]
