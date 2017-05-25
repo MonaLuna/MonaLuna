e5ec2ee559c84a753b7f4e9261c09b22c5187fae
MonaLuna <sara.kuhnert@hotmail.com>
2017-05-25 16:34:33 +0200

    Adds chaertli list view
    ...and defines its url endpoint
    ...and adds its template

--- chaertli/urls.py
+++ chaertli/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='chaertli-index'),
+    url(r'^list/$', views.ChaertliListView.as_view(), name='chaertli-list'),
 ]
--- chaertli/views.py
+++ chaertli/views.py
@@ -1,4 +1,13 @@
 from django.http import HttpResponse
+from django.views.generic import ListView
+
+from .models import Chaertli
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")	
+
+
+class ChaertliListView(ListView):
+
+	model = Chaertli
+	template_name = "chaertli/list.html"		
